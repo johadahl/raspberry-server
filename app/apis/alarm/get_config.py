aa4e40246ca5753659ec7638e3dbe3370eab243b
Johan Dahl <johan.dahl.92@hotmail.com>
2022-02-04 18:15:01 +0100

    gets and sets via api

--- app/apis/alarm/get_config.py
+++ app/apis/alarm/get_config.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 def get_config() -> dict[str, str|bool]:
     return {
-        "currentSetTime": "10:00",
+        "currentSetTime": "10:01",
         "isAlarmOn": True,
     }
--- app/routes/views.py
+++ app/routes/views.py
@@ -7,6 +7,12 @@ from app.apis.alarm.get_config import get_config
 from app.apis.alarm.set_config import set_config
 from app.core.auth import get_current_user
 
+from pydantic import BaseModel
+
+class AlarmConfig(BaseModel):
+    time: str
+    active: bool
+
 router = APIRouter()
 
 admin_html = os.path.join(os.path.dirname(__file__), 'templates', 'admin.html')
@@ -19,14 +25,16 @@ async def get_alarm_config(
 
 @router.post("/v1/alarm/", tags=["alarm"])
 async def set_alarm_config(
+    config: AlarmConfig,
     # auth: Depends = Depends(get_current_user),    // TODO: fix auth flow
 ) -> dict[str, int]:
+    print("CALLED", config)
     return set_config()
 
 
 @router.get(
     path='/',
-    description='The Admin Panel',
+    description='The Alarm Clock UI',
 )
 async def get_admin(
     request: Request,
